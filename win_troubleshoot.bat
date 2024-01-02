04b810c517da4e6c6cc7c9605fa287ff9d3f47bf
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-02 15:40:56 -0800

    more useful commands

---
+++ win_troubleshoot.bat
@@ -0,0 +1,4 @@
+REM - Scans and repairs corrupt or damaged system files within the Windows
+REM - image; helps resolve issues like: Failed updates, Performance slowdowns,
+REM - Error messages, App crashes;
+DISM /Online /Cleanup-Image /RestoreHealth
